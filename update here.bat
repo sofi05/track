1ee816bd961d909918caa0b3da6d2caad0beb188
Sofi <sofiss.sofia@gmail.com>
2025-09-22 20:10:05 -0300

    removed logo.webp and kept it as logo.png updated html

--- update here.bat
+++ update here.bat
@@ -1,3 +1,3 @@
 git add . 
-git commit -m "corrected march 7th version, changed haven't for don't, testing logo.webp as logo.png cropped"  
+git commit -m "removed logo.webp and kept it as logo.png updated html"  
 git push origin main  
